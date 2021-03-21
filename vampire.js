b6a636caca498fb567d2cf97a358a28d38e1a6e6
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-20 18:27:15 -0700

    Add files via upload

--- vampire.js
+++ vampire.js
@@ -10,23 +10,37 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
+    this.offspring.push(vampire); 
+    vampire.creator = this;
 
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true
+    }
+    else return false
   }
+ 
 
   /** Stretch **/
 
@@ -40,5 +54,24 @@ class Vampire {
   }
 }
 
+const original = new Vampire();
+const ansel = new Vampire ();
+const bart = new Vampire ();
+const mystery = new Vampire ();
+const elgort = new Vampire ();
+const sarah = new Vampire ();
+const andrew = new Vampire ();
+
+original.addOffspring(ansel);
+original.addOffspring(bart);
+original.addOffspring(mystery);
+
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+
+elgort.addOffspring(andrew);
+
+console.log(ansel.numberOfVampiresFromOriginal)
+console.log(original.isMoreSeniorThan(elgort));
 module.exports = Vampire;
 
